2b9af08ac5b9597e6dab1a6ba76323de5eb92e29
Coding Ape <autumind@gmail.com>
2015-07-22 14:31:18 +0800

    Create huffman_code.py

---
+++ huffman_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on Jul 22, 2015
+
+@author: shenzb
+'''
+from Node import Node 
+import Utils
+
+if __name__ == '__main__':
+
+    char_list = Utils.stringToPriorityQueue("hello, world!")
+    #print(char_list);
+
+    ''' Initialize PriorityQueue '''
+    nodeList = []
+    for ch, cnt in char_list:
+        # print(ch, cnt)
+        node = Node(ch, cnt)
+        nodeList.append(node)
