9481dbd03f8f840da2861efafe99117ab6f84263
Coding Ape <autumind@gmail.com>
2015-07-24 15:28:53 +0800

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -3,8 +3,9 @@
 '''
 Created on Jul 22, 2015
 
-@author: shenzb.fnst
+@author: shenzb
 '''
+from Node import Node
 
 def stringToPriorityQueue(text):
     '''
@@ -17,7 +18,31 @@ def stringToPriorityQueue(text):
     for char in chars:
         char_dict[char] = char_dict[char] + 1
 
-    ''' sort dict '''
+    ''' Get a priority queue list. '''
     char_list = sorted(char_dict.items(), lambda x, y: cmp(x[1], y[1]))
 
     return char_list
+
+def generateHuffmanTree(nodeList):
+    '''
+    Generate a Huffman Tree from a Priority Queue.
+    '''
+    nodeNew = nodeList[0].plus(nodeList[1])
+    nodeNew.setLeftChild(nodeList[0])
+    nodeNew.setRightChild(nodeList[1])
+
+    nodeList.append(nodeNew)
+    nodeList.pop(0)
+    nodeList.pop(0)
+
+    nodeList.sort(Node.compare)
+
+    if len(nodeList) == 1:
+        return nodeList[0]
+    return generateHuffmanTree(nodeList);
+
+def generateHuffmanCodes(huffmanTree):
+    '''
+    Generate Huffman codes from a Huffman Tree.
+    '''
+    pass
